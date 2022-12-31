bf1ef9f5ad05a1b2d9e557cbc24945c8a75663d7
Sahil Sonawane <iamsahilsonawane@gmail.com>
2022-12-31 17:12:42 +0530

    feat: 4 digit passcode update

--- lib/features/movies/widgets/enter_passcode_dialog.dart
+++ lib/features/movies/widgets/enter_passcode_dialog.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:latest_movies/core/constants/colors.dart';
 import 'package:latest_movies/core/services/shared_preferences_service.dart';
 import 'package:latest_movies/core/shared_widgets/button.dart';
-import 'package:latest_movies/core/shared_widgets/text_field.dart';
 import 'package:latest_movies/core/utilities/design_utility.dart';
+import 'package:pin_code_fields/pin_code_fields.dart';
 
 /// FOR [ADULT CONTENT] ONLY
 ///
@@ -36,47 +37,97 @@ class EnterPasscodeDialog extends HookConsumerWidget {
         child: Container(
           padding: const EdgeInsets.all(14),
           decoration: BoxDecoration(borderRadius: BorderRadius.circular(10)),
-          constraints: const BoxConstraints(maxWidth: 400),
-          child: Column(
+          constraints: const BoxConstraints(maxWidth: 300),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
             mainAxisSize: MainAxisSize.min,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
-              const Text("Enter passcode to access adult content"),
-              verticalSpaceRegular,
-              AppTextField(
-                hintText: "Enter passcode",
-                controller: passcodeCtrl,
-                focusNode: focusNode,
-                isPassword: true,
-                onEditingComplete: () {
-                  btnFocusNode.requestFocus();
-                },
+              Expanded(
+                flex: 4,
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    const Text("Enter passcode to access adult content"),
+                    verticalSpaceRegular,
+                    // AppTextField(
+                    //   // readOnly: true,
+                    //   keyboardType: const TextInputType.numberWithOptions(),
+                    //   inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+                    //   hintText: "Enter passcode",
+                    //   controller: passcodeCtrl,
+                    //   focusNode: focusNode,
+                    //   isPassword: true,
+                    //   onEditingComplete: () {
+                    //     btnFocusNode.requestFocus();
+                    //   },
+                    // ),
+                    PinCodeTextField(
+                      length: 4,
+                      obscureText: false,
+                      autoFocus: true,
+                      animationType: AnimationType.fade,
+                      appContext: context,
+                      pinTheme: PinTheme(
+                        shape: PinCodeFieldShape.box,
+                        borderRadius: BorderRadius.circular(5),
+                        fieldHeight: 60,
+                        fieldWidth: 60,
+                        activeFillColor: kPrimaryColor,
+                        activeColor: Colors.transparent,
+                        inactiveFillColor: Colors.transparent,
+                        inactiveColor: kPrimaryColor,
+                        selectedFillColor: kPrimaryColor.withOpacity(.2),
+                      ),
+                      cursorColor: Colors.white,
+                      backgroundColor: Colors.transparent,
+                      keyboardType: const TextInputType.numberWithOptions(),
+                      inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+                      animationDuration: const Duration(milliseconds: 300),
+                      enableActiveFill: true,
+                      controller: passcodeCtrl,
+                      // errorAnimationController: errorController,
+                      onCompleted: (v) {
+                        btnFocusNode.requestFocus();
+                      },
+                      onChanged: (value) {},
+                      beforeTextPaste: (text) => false,
+                    ),
+                    AppButton(
+                      text: "Access Content",
+                      onTap: () {
+                        String passcode = ref
+                                .read(sharedPreferencesServiceProvider)
+                                .sharedPreferences
+                                .getString(SharedPreferencesService
+                                    .adultContentPasscode) ??
+                            "";
+                        if (passcodeCtrl.text == passcode) {
+                          Navigator.pop(context, true);
+                        } else {
+                          Navigator.pop(context, false);
+                        }
+                      },
+                      prefix: const Icon(Icons.login),
+                      focusNode: btnFocusNode,
+                    ),
+                    AppButton(
+                      text: "Go Back",
+                      onTap: () {
+                        Navigator.pop(context, false);
+                      },
+                    ),
+                  ],
+                ),
               ),
-              verticalSpaceRegular,
-              AppButton(
-                text: "Access Content",
-                onTap: () {
-                  String passcode = ref
-                          .read(sharedPreferencesServiceProvider)
-                          .sharedPreferences
-                          .getString(
-                              SharedPreferencesService.adultContentPasscode) ??
-                      "";
-                  if (passcodeCtrl.text == passcode) {
-                    Navigator.pop(context, true);
-                  } else {
-                    Navigator.pop(context, false);
-                  }
-                },
-                prefix: const Icon(Icons.login),
-                focusNode: btnFocusNode,
-              ),
-              AppButton(
-                text: "Go Back",
-                onTap: () {
-                  Navigator.pop(context, false);
-                },
-              ),
+              // horizontalSpaceMedium,
+              // Expanded(
+              //   flex: 2,
+              //   child: AppOnScreenKeyboard(
+              //     onlyNumbers: true,
+              //     onValueChanged: (value) => passcodeCtrl.text = value,
+              //   ),
+              // ),
             ],
           ),
         ),
--- lib/features/movies/widgets/set_passcode_dialog.dart
+++ lib/features/movies/widgets/set_passcode_dialog.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:latest_movies/core/constants/colors.dart';
 import 'package:latest_movies/core/shared_widgets/button.dart';
-import 'package:latest_movies/core/shared_widgets/text_field.dart';
 import 'package:latest_movies/core/utilities/design_utility.dart';
+import 'package:pin_code_fields/pin_code_fields.dart';
 
 import '../../../core/services/shared_preferences_service.dart';
 
@@ -32,23 +33,53 @@ class SetPasscodeDialog extends HookConsumerWidget {
         child: Container(
           padding: const EdgeInsets.all(14),
           decoration: BoxDecoration(borderRadius: BorderRadius.circular(10)),
-          constraints: const BoxConstraints(maxWidth: 400),
+          constraints: const BoxConstraints(maxWidth: 300),
           child: Column(
             mainAxisSize: MainAxisSize.min,
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
               const Text("Set a passcode to access adult content"),
               verticalSpaceRegular,
-              AppTextField(
-                hintText: "Enter passcode",
+              // AppTextField(
+              //   hintText: "Enter passcode",
+              //   controller: passcodeCtrl,
+              //   focusNode: focusNode,
+              //   isPassword: true,
+              //   onEditingComplete: () {
+              //     btnFocusNode.requestFocus();
+              //   },
+              // ),
+              PinCodeTextField(
+                length: 4,
+                obscureText: false,
+                autoFocus: true,
+                animationType: AnimationType.fade,
+                appContext: context,
+                pinTheme: PinTheme(
+                  shape: PinCodeFieldShape.box,
+                  borderRadius: BorderRadius.circular(5),
+                  fieldHeight: 60,
+                  fieldWidth: 60,
+                  activeFillColor: kPrimaryColor,
+                  activeColor: Colors.transparent,
+                  inactiveFillColor: Colors.transparent,
+                  inactiveColor: kPrimaryColor,
+                  selectedFillColor: kPrimaryColor.withOpacity(.2),
+                ),
+                cursorColor: Colors.white,
+                backgroundColor: Colors.transparent,
+                keyboardType: const TextInputType.numberWithOptions(),
+                inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+                animationDuration: const Duration(milliseconds: 300),
+                enableActiveFill: true,
                 controller: passcodeCtrl,
-                focusNode: focusNode,
-                isPassword: true,
-                onEditingComplete: () {
+                // errorAnimationController: errorController,
+                onCompleted: (v) {
                   btnFocusNode.requestFocus();
                 },
+                onChanged: (value) {},
+                beforeTextPaste: (text) => false,
               ),
-              verticalSpaceRegular,
               AppButton(
                 text: "Set Passcode & Enter",
                 onTap: () async {
