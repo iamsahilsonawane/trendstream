e54a0cf4045b382944ccb51daee3c04dc237dd71
Sahil Sonawane <iamsahilsonawane@gmail.com>
2024-02-13 17:38:56 +0530

    chore(deps): version bumps

--- lib/core/shared_widgets/image.dart
+++ lib/core/shared_widgets/image.dart
@@ -1,5 +1,6 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_blurhash/flutter_blurhash.dart';
 import 'package:octo_image/octo_image.dart';
 
 class AppImage extends StatelessWidget {
@@ -20,7 +21,7 @@ class AppImage extends StatelessWidget {
   Widget build(BuildContext context) {
     return OctoImage(
       image: CachedNetworkImageProvider(imageUrl),
-      placeholderBuilder: OctoPlaceholder.blurHash(
+      placeholderBuilder: blurHashPlaceholderBuilder(
         r"dOI.]h--}ln$]WNZIUWXI_I:ROof=|s=${R*R+$zNGbE",
       ),
       fit: fit,
@@ -28,5 +29,15 @@ class AppImage extends StatelessWidget {
       errorBuilder:
           errorWidget ?? (context, url, error) => const Icon(Icons.error),
     );
+  }
+
+  OctoPlaceholderBuilder blurHashPlaceholderBuilder(String hash,
+      {BoxFit? fit}) {
+    return (context) => SizedBox.expand(
+          child: Image(
+            image: BlurHashImage(hash),
+            fit: fit ?? BoxFit.cover,
+          ),
+        );
   }
 }
