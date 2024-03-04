7ba29f568c12e513cb03a11a8cf59d0cadb6c042
Sahil Sonawane <iamsahilsonawane@gmail.com>
2024-03-04 20:52:48 +0530

    feat: hide status-bar for android view

--- android/app/src/main/java/com/example/latest_movies/GuideActivity.kt
+++ android/app/src/main/java/com/example/latest_movies/GuideActivity.kt
@@ -1,5 +1,6 @@
 package com.example.latest_movies
 
+import android.view.View // Add this line
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.jakewharton.threetenabp.AndroidThreeTen
@@ -7,6 +8,18 @@ import com.jakewharton.threetenabp.AndroidThreeTen
 class GuideActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // Hide the status bar.
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+        // Remember that you should never show the action bar if the
+        // status bar is hidden, so hide that too if necessary.
+        actionBar?.hide()
+
+        // Hide the status bar.
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+        // Remember that you should never show the action bar if the
+        // status bar is hidden, so hide that too if necessary.
+        actionBar?.hide()
+
         AndroidThreeTen.init(this)
         setContentView(R.layout.activity_guide)
         supportActionBar?.hide()
